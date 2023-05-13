15df56f2e4ba9943c24fbec55bf38eba30b21c29
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-13 03:34:00 +0200

    add Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { Container } from "./Container";
+import { Footer } from "./Footer";
 
 function App() {
-  return <Container />;
+  return (
+    <>
+      <Container />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Footer/index.js
@@ -0,0 +1,5 @@
+import { StyledLink, StyledFooter } from "./styled";
+
+export const Footer = () => (
+    <StyledFooter>Challenge by <StyledLink href="https://www.frontendmentor.io/home" target="_blank" rel="noreferrer">Frontend Mentor</StyledLink>. Coded by <StyledLink href="https://github.com/Siedemus" target="_blank" rel="noreferrer">Siedemus</StyledLink>.</StyledFooter>
+);
