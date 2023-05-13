fe2f8bfb06661c2ee2abcbd2bbb990183a8f11dd
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-13 02:13:15 +0200

    add advice styles

--- src/Container/Advice/styled.js
+++ src/Container/Advice/styled.js
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 
-const AdviceHeader = styled.h1``;
+const AdviceHeader = styled.h1`
+color: ${({theme}) => theme.colors.neonGreen};
+font-size: 13px;
+text-transform: uppercase;
+letter-spacing: 3px;
+padding-top: 20px;
+`;
 
-const StyledAdvice = styled.p``;
+const StyledAdvice = styled.p`
+color: ${({theme}) => theme.colors.lightCyan};
+font-size: 28px;
+padding: 20px 20px 40px 20px;
+font-weight: 800;
+text-align: center;
+`;
 
-const BorderImage = styled.img``;
+const BorderImage = styled.img`
+padding-bottom: 20px;
+`;
 
 export { AdviceHeader, StyledAdvice, BorderImage };
