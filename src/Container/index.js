7fe507b2fc50e3d5ba6911899e58409869317b00
Bartosz Sakiewa <barsak1177@gmail.com>
2023-06-08 13:44:17 +0200

    change loader

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,21 +1,28 @@
 import { useState } from "react";
 import { Advice } from "./Advice";
 import { Button } from "./Button";
-import { StyledContainer } from "./styled";
+import { AdviceContainer, LoadinContainer } from "./styled";
 import { useFetchAdvice } from "./useFetchAdvice";
+import { PacmanLoader as Loader } from "react-spinners";
 
 export const Container = () => {
   const [trigger, setTrigger] = useState(false);
   const data = useFetchAdvice(trigger);
 
-  const isDataFalse = data ? false : true;
+  const isData = data ? false : true;
 
   return (
     <>
-      <StyledContainer loading={+isDataFalse}>
-        <Advice data={data} />
-        <Button setTrigger={setTrigger} />
-      </StyledContainer>
+      {data ? (
+        <AdviceContainer loading={+isData}>
+          <Advice data={data} />
+          <Button setTrigger={setTrigger} />
+        </AdviceContainer>
+      ) : (
+        <LoadinContainer>
+          <Loader color="#52ffa8" size={30} />
+        </LoadinContainer>
+      )}
     </>
   );
 };
